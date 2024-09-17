fdebd855a46378fe57104388caf0c966f38ac7b1
Nicolle Grasiele pereira de souza <146836291+pereiranicolle@users.noreply.github.com>
2024-09-17 16:05:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável??',
+    'Variável é a característica de interesse que é medida em cada elemento da amostra ou população. Como o nome diz, seus valores variam de elemento para elemento. As variáveis podem ter valores numéricos ou não numéricos'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é o efeito Coriolis e como ele afeta os padrões climáticos?',
+    'É por causa da força de Coriolis que grandes camadas de ar entram em movimento de rotação originando os ciclones;ciclones que giram no hemisfério sul'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a diferença entre "REST" e "GraphQL"?',
+    'Uma API REST é um conceito arquitetônico para comunicação de aplicações. Por outro lado, GraphQL é uma especificação, uma linguagem de consulta de API e um conjunto de ferramentas.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual é a diferença entre os artigos definidos e indefinidos em inglês?',
+    'Por exemplo, os artigos definidos não acompanham nomes próprios, cores, esportes, nome de lugares, disciplinas, línguas e outros termos. Além disso, a gramática inglesa proíbe o uso do artigo indefinido com substantivos não contáveis. Confira a seguir mais pontos de atenção sobre os artigos em inglês'
+)
